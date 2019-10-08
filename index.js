af1c86c788d2d3822990b624c4a57518eebe8756
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-10-08 17:33:25 +0200

    Add mongo model classes

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const graphqlHttp = require('express-graphql');
 const {
   buildSchema
 } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/events').default.default;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const events = [];
 
 app.use(express.json());
 
@@ -38,24 +39,36 @@ const schema = buildSchema(`
       query: RootQuery
       mutation: RootMutation
     }
-  `)
-
-class Event {
-  constructor({title, description, price}) {
-    this._id = Math.random().toString();
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.date = new Date().toISOString();
-  }
-}
+  `);
 
 const root = {
-  events: () => events,
+  events: () => {
+    return Event.find().then(events => {
+      return events.map(event => {
+        return {
+          ...event._doc,
+          _id: event.id
+        }
+      });
+    }).catch(err => {
+      throw err;
+    })
+  },
   createEvent: (args) => {
-    const event = new Event(args.input);
-    events.push(event);
-    return event;
+    const event = new Event({
+      ...args.input,
+      date: new Date(new Date().toISOString()),
+    });
+    return event.save()
+      .then(result => {
+        return {
+          ...result._doc,
+          _id: result._doc._id.toString()
+        }
+      })
+      .catch(err => {
+        throw err;
+      });
   }
 }
 
@@ -65,4 +78,12 @@ app.use('/graphql', graphqlHttp({
   graphiql: true,
 }));
 
-app.listen(PORT, () => console.log(`App runs on port ${PORT}`));
+const URI = `mongodb+srv://${process.env.MONGO_USER}:${
+              process.env.MONGO_PASS}@cluster0-jrk3z.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`App runs on port ${PORT}`));
+}).catch(err => console.log(err));
